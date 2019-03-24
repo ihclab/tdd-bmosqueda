972d4d9c2b80bb9d24e5e7aad3f563938ec9d5b9
Brandon Mosqueda <bmosqueda@ucol.mx>
2019-03-23 18:19:12 -0600

    Paso 3: Primer caso de prueba programado (Ordenar arreglo ordenado)

--- Ordenamiento.Pruebas/PruebasOrdenamieto.cs
+++ Ordenamiento.Pruebas/PruebasOrdenamieto.cs
@@ -3,11 +3,17 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Ordenamiento.Pruebas
 {
     [TestClass]
-    public class UnitTest1
+    public class PruebasOrdenamiento
     {
+        private int[] salidaEsperada = new int[] {1, 2, 3, 4, 5};
+        private Ordenamiento.Burbuja ordenar = new Ordenamiento.Burbuja();
+        
         [TestMethod]
-        public void TestMethod1()
+        public void PruebaOrdenado()
         {
+            int[] entradas = new int[] {1, 2, 3, 4, 5};
+            ordenar.Ordenar(entradas);
+            CollectionAssert.AreEqual(salidaEsperada, entradas);
         }
     }
 }
