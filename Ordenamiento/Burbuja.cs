2e7e7f35c09b12dca81eeafe7dc847d7b5d0046f
Brandon Mosqueda <bmosqueda@ucol.mx>
2019-03-23 18:06:25 -0600

    Estructura de la clase Ordenamiento

---
+++ Ordenamiento/Burbuja.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ordenamiento
+{
+    public class Burbuja
+    {
+    	public void Ordenar(int[] array) 
+    	{
+    		throw new NotImplementedException("Crear los casos de prueba");
+    	}
+    }
+}
